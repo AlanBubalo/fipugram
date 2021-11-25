689f3855804c78b841b77225ccc69a9c16f1effc
Alan Bubalo <alan.buba5@gmail.com>
2021-11-25 19:51:26 +0100

    First commit

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <div class="jumbotron">
+      <h1 class="display-4">Welcome to Vue</h1>
+      <p class="lead">You can login by clicking the "Login" button.</p>
+      <hr class="my-4" />
+      <p>Or if you don't have an account, sign up.</p>
+    </div>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
---
+++ src/views/Signup.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="about">
+    <h1>This is a sign up page</h1>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm"></div>
+        <div class="col-sm">
+          <form>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Name</label>
+              <input
+                type="text"
+                class="form-control"
+                id="exampleInputName1"
+                placeholder="Enter your name"
+              />
+            </div>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Email address</label>
+              <input
+                type="email"
+                class="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                placeholder="Enter email"
+              />
+              <small id="emailHelp" class="form-text text-muted"
+                >We'll never share your email with anyone else.</small
+              >
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Password</label>
+              <input
+                type="password"
+                class="form-control"
+                id="exampleInputPassword1"
+                placeholder="Password"
+              />
+            </div>
+            <div class="form-group">
+              <label for="exampleInputConfirmPassword1">Confirm Password</label>
+              <input
+                type="password"
+                class="form-control"
+                id="exampleInputConfirmPassword1"
+                placeholder="Confirm password"
+              />
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
+        </div>
+        <div class="col-sm"></div>
+      </div>
+    </div>
+  </div>
+</template>
